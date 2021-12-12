36916887fd4b2ed53c1d368f6f93afdc3ed8fa5b
Artur Cavalcante de Jesus <56032621+arturcavalcantej@users.noreply.github.com>
2021-12-12 12:10:14 -0300

    feature: encapsulamento de dados sensiveis

--- back_end/desafio/settings.py
+++ back_end/desafio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-86^w7a99jiz-3b6$+u!g$)y5o7mnv86fo2ic(^g@t2+mz^)iek'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -84,15 +85,15 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'clientes-db',
+        'NAME': config('DB_NAME'),
 
-        'USER': 'admin',
+        'USER': config('DB_USER'),
 
-        'PASSWORD': 'admin1234',
+        'PASSWORD': config('DB_PASS'),
 
-        'HOST': 'banco-django',
+        'HOST': config('DB_HOST'),
 
-        'PORT': '5432',
+        'PORT': config('DB_PORT'),
 
     }
 }
